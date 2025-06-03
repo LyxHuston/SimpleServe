9df839beaadf6867276c56aa72e1a8340ebdfae7
Lyx <lyx@hustonfam.net>
2025-06-03 00:19:37 -0400

    refactor to using piping

--- src/serve.rs
+++ src/serve.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf, process::Command, fs::File, io::{Read, Write}};
+use std::{fs::File, io::{Read, Write}, path::PathBuf, process::{Child, Command, Stdio}};
 use hyper::{
 	Request, Response,
 	body::{Bytes, Incoming}
@@ -12,13 +12,24 @@ use cmd_lib::run_fun;
 
 use tempfile::tempfile;
 
-enum ResultType {
+enum ProcessingState {
 	ErrorCode(u16),
 	InternalError(u16, String),
-	Body(Response<File>),
+	Static(Response<Vec<u8>>),
+	Chain(Vec<Child>),
 	HttpError(Error)
 }
 
+use ProcessingState::*;
+
+//#[inline(always)]
+fn halt_processing(proc: &mut ProcessingState) {
+	let Chain(proc) = proc else {return};
+	for child in proc {
+		let _ = child.kill();
+		let _ =child.wait();
+	}
+}
 
 /// due to a restriction of unix, exit codes of a program are only a u8,
 /// which is not large enough for all HTTP status codes.  So, here's a
@@ -50,21 +61,32 @@ fn to_exit_code(res: Option<i32>) -> u16 {
 }
 
 
+fn file_with(data: &[u8]) -> Option<File> {
+	let mut tmp = tempfile().ok()?;
+	tmp.write_all(data).ok()?;
+	Some(tmp)
+}
+
 // args passed to commands are uri_path, METHOD and then mappings (server does not get fragment)
-fn handle_file(file: &PathBuf, stdin: File, params: &Vec<String>) -> ResultType {
+fn handle_file(file: &PathBuf, mut prev_state: ProcessingState, params: &Vec<String>) -> ProcessingState {
 	// there are many time-of-check time-of-use race conditions here.
 	// this is fine, because it's not expecting to be serving from
 	// directories that are changing frequently
-	if !file.exists() {
-		ResultType::ErrorCode(404)
+	if let HttpError(e) = prev_state {
+		HttpError(e) // just forward it.  Don't know and isn't my responsibility to handle these
+	} else if !file.exists() {
+		halt_processing(&mut prev_state);
+		ErrorCode(404)
 	} else if file.is_dir() {
 		// I am a teapot: I am a dir
-		ResultType::ErrorCode(418)
+		halt_processing(&mut prev_state);
+		ErrorCode(418)
 	} else if file.is_executable() {
 		let Some(work_dir) = file.parent() else {
 			// if it cannot determine the parent, that means it's already at root.  Which is bad.
 			// and not just because this shouldn't be running on a dir
-			return ResultType::InternalError(
+			halt_processing(&mut prev_state);
+			return InternalError(
 				500,
 				format!(
 					"Could not determine parent folder of {}",
@@ -72,58 +94,62 @@ fn handle_file(file: &PathBuf, stdin: File, params: &Vec<String>) -> ResultType 
 				)
 			);
 		};
-		let Ok(headers)	= tempfile() else {return ResultType::InternalError(
-			500, String::from("Could not create header tempfile"))
+		let Ok(headers)	= tempfile() else {
+			halt_processing(&mut prev_state);
+			return InternalError(
+				500, String::from("Could not create header tempfile"))
 		};
-		let Ok(mut output)	= tempfile() else {return ResultType::InternalError(
-			500, String::from("Could not create output tempfile"))
+		let (input_opt, mut prev_chain) = match prev_state {
+			Chain(mut v) => (v
+				.last_mut()
+				.map(|c| c.stdout.take())
+				.unwrap_or(None)
+				.map(|c| Stdio::from(c)), v),
+			Static(b) => (file_with(b.body()).map(Stdio::from), Vec::new()),
+			_ => (tempfile().ok().map(Stdio::from), Vec::new())
 		};
-		// TODO: use piping and child processes instead of blocking?
-		// because of how Command stdout works, might actually be easier to do so.
-		let Ok(mut child) = Command::new(file)
+		let Some(input) = input_opt else {
+			for mut c in prev_chain {
+				c.kill();
+			}
+			return InternalError(
+				500, format!("Could not ascertain input from previous processing state"))
+		};
+		let Ok(child) = Command::new(file)
 			.current_dir(work_dir)
 			.args(params)
-			.stdin(stdin)
+			.stdin(input)
 			.stderr(headers)
-			.stdout(output)
-			.spawn() else {return ResultType::InternalError(
-				500, format!("Error running command {}", file.to_string_lossy()))
-			};
-		let full_output = child.wait_with_output();
-		let Some(mut header_out) = child.stderr else {unreachable!()};
-		let Some(mut data_out) = child.stdout else {unreachable!()};
-		let code = to_exit_code(Some(1));
-		if 200 <= code && code < 300 {
-			let mut header_string = String::new();
-			let Ok(_) = header_out.read_to_string(&mut header_string) else {
-				return ResultType::InternalError(
-					500, String::from("Error reading from header temporary file!")
-				)
-			};
-			header_string.split("\n").filter_map(|s| s.split_once("=")).fold(
-				Builder::new().status(code),
-				|b, (k, v)| {
-					b.header(k, v)
+			.stdout(Stdio::piped())
+			.spawn() else {
+				for mut c in prev_chain {
+					c.kill();
 				}
-			).body(child.wa).map_or_else(
-				|err|	ResultType::HttpError(err),
-				|ok|	ResultType::Body(ok)
-			)
-		} else {
-			ResultType::ErrorCode(code)
-		}
+				return InternalError(
+					500, format!("Error running command {}", file.to_string_lossy()))
+			};
+		prev_chain.push(child);
+		Chain(prev_chain)
 	} else {
 		// if exists, not executable, not a folder, return 200, Content-type mime-type, and the file 
-		let Ok(open_file) = File::open(file) else {
-			// couldn't open file but it does exist
-			return ResultType::InternalError(
+		halt_processing(&mut prev_state); // should user be *allowed* to funnel a chain process
+										// into a static file?
+		let Ok(mut open_file) = File::open(file) else {
+			return InternalError(
 				500,
 				format!("Couldn't open file {}", file.to_string_lossy())
 			);
 		};
+		let mut data = Vec::new();
+		let Ok(_) = open_file.read_to_end(&mut data) else {
+			return InternalError(
+				500,
+				format!("Couldn't read file {}", file.to_string_lossy())
+			);
+		};
 		let Ok(mimetype) = run_fun!(file -ib $file) else {
 			// couldn't get mimetype
-			return ResultType::InternalError(
+			return InternalError(
 				500,
 				format!("Error getting mimetype of {}", file.to_string_lossy())
 			);
@@ -131,10 +157,10 @@ fn handle_file(file: &PathBuf, stdin: File, params: &Vec<String>) -> ResultType 
 		Builder::new()
 			.status(200)
 			.header("Content-Type", mimetype)
-			.body(open_file)
+			.body(data)
 			.map_or_else(
-				|err|	ResultType::HttpError(err),
-				|ok|	ResultType::Body(ok)
+				|err|	HttpError(err),
+				|ok|	Static(ok)
 			)
 	}
 }
@@ -142,21 +168,45 @@ fn handle_file(file: &PathBuf, stdin: File, params: &Vec<String>) -> ResultType 
 
 fn handle_layer(
 	curr_layer: &mut PathBuf, remaining_layers: &[String],
-	params: &mut Vec<String>, incoming_body: &File
-) -> ResultType {
+	params: &mut Vec<String>, incoming_body: ProcessingState
+) -> ProcessingState {
 	if remaining_layers.is_empty() {
-		if curr_layer.is_dir() {
-			curr_layer.push(".index");
-		}
+		return handle_file(curr_layer, incoming_body, params);
 	} else if remaining_layers[0].starts_with(".") {
 		// hide hidden files/directories and prevent escape through '..'
-		return ResultType::ErrorCode(403);
+		return ErrorCode(403);
 	} else {
 		curr_layer.push(remaining_layers[0].clone());
 		handle_layer(curr_layer, &remaining_layers[1..], params, incoming_body);
 		curr_layer.pop();
 	}
-	return ResultType::ErrorCode(404);
+	return ErrorCode(404);
+}
+
+fn resolve_to_response(status: ProcessingState) -> Result<Response<Full<Bytes>>, Error> {
+	match status {
+		ErrorCode(e) =>
+			Builder::new().status(e).body(
+				Full::new(Bytes::from(format!("Error {}: That's all we know", e)))
+			),
+		InternalError(e, msg) => {
+			println!("{}", msg);
+			Builder::new().status(e).body(
+				Full::new(Bytes::from(format!("Error {}: That's all we know", e)))
+			)
+		}
+		Static(b) =>
+			b.headers().iter().fold(
+				Builder::new().status(b.status()),
+				|build, (key, val)| build.header(key, val)
+			)
+			.header("Content-Length", b.body().len())
+			.body(Full::new(Bytes::from(b.body().clone())))
+		,
+		HttpError(e) => Err(e),
+		Chain(c) =>
+			unimplemented!()
+	}
 }
 
 pub async fn serve(req: Request<Incoming>, path: PathBuf)
@@ -187,37 +237,6 @@ pub async fn serve(req: Request<Incoming>, path: PathBuf)
 			Full::new(Bytes::from(format!("Error 500: That's all we know")))
 		)
 	};
-	inp.write_all(&*body_data.to_bytes());
 	// handle it, then go over the output
-	match handle_layer(&mut path, &layers[..], &mut params, &inp) {
-		ResultType::ErrorCode(e) =>
-			Builder::new().status(e).body(
-				Full::new(Bytes::from(format!("Error {}: That's all we know", e)))
-			),
-		ResultType::InternalError(e, msg) => {
-			println!("{}", msg);
-			Builder::new().status(e).body(
-				Full::new(Bytes::from(format!("Error {}: That's all we know", e)))
-			)
-		}
-		ResultType::Body(b) => {
-			let mut bytes: Vec<u8> = Vec::new();
-			let Ok(size) = b.body().read_to_end(&mut bytes) else {
-				println!("Error reading back from file");
-				return Builder::new().status(500).body(
-					Full::new(Bytes::from(format!("Error 500: That's all we know")))
-				)
-			};
-			b.headers().iter().fold(
-				Builder::new().status(b.status()),
-				|build, (key, val)| build.header(key, val)
-			)
-			.header("Content-Length", size)
-			.body(Full::new(Bytes::from(bytes)))
-		},
-		ResultType::HttpError(e) => Err(e)
-	}
-	// need to:
-	// - find request targets, and handle them based on type
-	// - errors handling
+	resolve_to_response(handle_layer(&mut path, &layers[..], &mut params, Static(body_data.into())))
 }
